7b0f1e1c6e2b27f9b7aa10247b3beb6fb5021b02
Keyur-Dobariya <139529881+Keyur-Dobariya@users.noreply.github.com>
2025-07-11 11:17:08 +0530

    Add files via upload

--- app/InnerAppLayout.jsx
+++ app/InnerAppLayout.jsx
@@ -1,57 +1,64 @@
 'use client';
 
-
-import { useEffect, useRef } from 'react';
-import { useRouter } from 'next/navigation';
+import {Suspense, useEffect, useRef} from 'react';
+import {usePathname, useRouter} from 'next/navigation';
 import { useAppData } from './masterData/AppDataContext';
 import { getLocalData, storeLoginData } from './dataStorage/DataPref';
 import appKey from './utils/appKey';
 import apiCall, { HttpMethod } from './api/apiServiceProvider';
 import { endpoints } from './api/apiEndpoints';
 import pageRoutes from './utils/pageRoutes';
-import {useLoading} from "./layout";
 import {LoadingComponent} from "./components/LoadingComponent";
+import {detectPlatform} from "./utils/utils";
 
 export default function InnerAppLayout({ children }) {
     const appDataContext = useAppData();
-    const {isLoading, setIsLoading} = useLoading();
+    const {isLoading, setIsLoading} = useRef(false);
     const router = useRouter();
-    const hasFetchedDataRef = useRef(false);
 
-    useEffect(() => {
-        const fetchMasterData = async () => {
-            try {
-                const response = await apiCall({
-                    method: HttpMethod.GET,
-                    url: endpoints.getMasterData,
-                    setIsLoading,
-                    showSuccessMessage: false,
-                });
+    const fetchMasterData = async () => {
+        try {
+            const pathname = usePathname();
+            const response = await apiCall({
+                method: HttpMethod.GET,
+                url: endpoints.getMasterData,
+                setIsLoading,
+                showSuccessMessage: false,
+            });
 
-                if (response?.data) {
-                    appDataContext.setAllMasterData(response.data);
-                    storeLoginData(response?.data?.loginUserData, false);
+            if (response?.data) {
+                appDataContext.setAllMasterData(response.data);
+                storeLoginData(response?.data?.loginUserData, false);
 
+                if(pathname === '/') {
                     const token = getLocalData(appKey.jwtToken);
                     if (token) {
                         router.push(pageRoutes.dashboard);
                     } else {
-                        router.push(pageRoutes.signupPage);
+                        router.push(pageRoutes.loginPage);
                     }
+                } else {
+                    router.push(pathname);
                 }
-            } catch (error) {
-                console.error('Failed to fetch master data:', error);
             }
-        };
+        } catch (error) {
+            console.error('Failed to fetch master data:', error);
+        }
+    };
 
-        const isLoggedIn = getLocalData(appKey.isLogin) === 'true';
-        if (isLoggedIn && !hasFetchedDataRef.current) {
-            hasFetchedDataRef.current = true;
-            fetchMasterData();
+    useEffect(() => {
+        const userAgent = navigator.userAgent;
+        const detected = detectPlatform(userAgent);
+
+        if(!detected.isElectron) {
+            const isLoggedIn = getLocalData(appKey.isLogin) === 'true';
+            if (isLoggedIn) {
+                fetchMasterData();
+            }
+        } else {
+            router.push(pageRoutes.tracker);
         }
     }, []);
 
-    return isLoading ? <div className="w-full h-full flex justify-center items-center">
-        <LoadingComponent />
-    </div> : children;
+    return children;
 }
